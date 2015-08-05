a23de9c0fec5235d67475f0f56fc6456e0333923
Dariusz Łukasik <dariuszz.lukasik@gmail.com>
2015-08-05 11:27:28 +0200

    third commit

--- src/Output.java
+++ src/Output.java
@@ -7,6 +7,15 @@ public class Output {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hej");
+		int c=10; 
+		switch(c)
+		{
+			case 10:
+			System.out.println("Podano 10");
+			break;
+			case 11:
+			System.out.println("Podano 11");
+		}
 
 	}
 
