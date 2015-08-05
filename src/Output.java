8d283c2ecf542e50210e61c55d1b2867c6f5e62b
Dariusz Łukasik <dariuszz.lukasik@gmail.com>
2015-08-05 11:24:38 +0200

    twice commit

--- src/Output.java
+++ src/Output.java
@@ -5,7 +5,8 @@ public class Output {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("Hello");
+		System.out.println("Hello");
+		System.out.println("Hej");
 
 	}
 
